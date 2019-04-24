cdb485cf75b37321c49de615e37ae6e838a50aa2
unknown <xrage6@gmail.com>
2019-04-24 21:59:56 +0200

    commit

--- coffeebar/src/component/News/Index.js
+++ coffeebar/src/component/News/Index.js
@@ -9,28 +9,22 @@ class News extends Component {
                 <div className="gallery">
                     <div className="card">
                         <img className="card_img" src={require("../img/coffee1.jpg")} alt=""/>
-                        <div className="text_block">
-                            <p>
-                                Zachwyca waniliowym smakiem i delikatnym aromatem. Jej głęboki waniliowy smak to prawdziwy raj dla podniebienia.
-                            </p>
+                        <div className="card_text">
+                        Zachwyca waniliowym smakiem i delikatnym aromatem. Jej głęboki waniliowy smak to prawdziwy raj dla podniebienia.
                         </div>
                         <div className="card_head">Waniliowe Cappuccino</div>
                     </div>
                     <div className="card">
                         <img className="card_img" src={require("../img/coffee2.jpg")} alt=""/>
-                        <div className="text_block">
-                            <p>
+                        <div className="card_text">
                             Wyjątkowy posmak palonego karmelu idealnie uzupełnia aromatyczne espresso połączone z gładką konsystencją spienionego mleka.
-                            </p>
                         </div>
                         <div className="card_head">Carmel Latte</div>
                     </div>
                     <div className="card">
                         <img className="card_img" src={require("../img/coffee3.jpg")} alt=""/>
-                        <div className="text_block">
-                            <p>
+                        <div className="card_text">
                             Pyszna kawa na bazie belgijskiej czekolady z dodatkiem bitej śmietany.
-                            </p>
                         </div>
                         <div className="card_head">Czekoladowa rozkosz</div>
                     </div>
