afa6c210a43dc46eafbe3a3298bf7f96832cd0e1
unknown <xrage6@gmail.com>
2019-06-11 00:01:26 +0200

    Add styles in Google maps

--- src/component/GoogleMap/Index.js
+++ src/component/GoogleMap/Index.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
 import './Index.scss';
 
-export class GoogleMap extends Component {
+export class GoogleMap extends Component {   
     render() {
         return ( 
             <div className='google_map'>
                 <Map
                 google={this.props.google}
                 zoom={14}
-                initialCenter={{
+                style={{width: '100%', position: 'absolute', left: '30px', bottom: '30px'}}                initialCenter={{
                 lat: 52.234899,
                 lng: 20.992110
                 }}
