cb424c33a6b72511d29d3b2ae578ffc41fb3565b
unknown <xrage6@gmail.com>
2019-06-10 22:52:29 +0200

    Change HTML element in About

--- src/component/About/Index.js
+++ src/component/About/Index.js
@@ -5,29 +5,24 @@ import './Index.scss';
 export default function About(props){
     return ( 
         <div id="about" className="about contain">
-        <div className="title_about">
-            <h2>{props.data.about}</h2>
-            <span>{props.data.heading_about}</span>
-        </div>
+            <section>
+                <h2>{props.data.about}</h2>
+                <p>{props.data.heading_about}</p>
+            </section>
+
             <Container>
                 <Row>
                     <Col>
-                    <div>
                         <img className="img_about" src={require(`../../assets/${props.data.img_about1}`)} alt=''/>
                         <label className="text_about">{props.data.text_about1}</label>
-                    </div>
                     </Col>
                     <Col>
-                    <div>
                         <label className="text_about">{props.data.text_about2}</label>
                         <img className="img_about" src={require(`../../assets/${props.data.img_about2}`)} alt=''/>
-                    </div>
                     </Col>
                     <Col >
-                    <div>
                         <img className="img_about" src={require(`../../assets/${props.data.img_about3}`)} alt=''/>
                         <label className="text_about">{props.data.text_about3}</label>
-                    </div>
                     </Col>
                 </Row>
             </Container>
