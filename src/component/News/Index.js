ab5a5c47af0ff003bb7eabf03a1e9c9149c4f8e6
unknown <xrage6@gmail.com>
2019-06-11 00:11:37 +0200

    Change HTML element in News

--- src/component/News/Index.js
+++ src/component/News/Index.js
@@ -6,21 +6,21 @@ export default function News(props) {
         <div id="news" className="news contain">
             <h2>{props.data.news}</h2>
             <div className="gallery">
-                <div className="card">
-                    <img className="card_img" src={require(`../../assets/${props.data.img_slider1}`)} alt=""/>
-                    <div className="card_head">{props.data.title_slider1}</div>
-                    <div className="card_text">{props.data.text_slider1}</div>
-                </div>
-                <div className="card">
-                    <img className="card_img" src={require(`../../assets/${props.data.img_slider2}`)} alt=""/>
-                    <div className="card_head">{props.data.title_slider2}</div>
-                    <div className="card_text">{props.data.text_slider2}</div>
-                </div>
-                <div className="card">
-                    <img className="card_img" src={require(`../../assets/${props.data.img_slider3}`)} alt=""/>
-                    <div className="card_head">{props.data.title_slider3}</div>
-                    <div className="card_text">{props.data.text_slider3}</div>
-                </div>
+                <section className="card">
+                    <img src={require(`../../assets/${props.data.img_slider1}`)} alt=""/>
+                    <h3>{props.data.title_slider1}</h3>
+                    <p>{props.data.text_slider1}</p>
+                </section>
+                <section className="card">
+                    <img src={require(`../../assets/${props.data.img_slider2}`)} alt=""/>
+                    <h3>{props.data.title_slider2}</h3>
+                    <p>{props.data.text_slider2}</p>
+                </section>
+                <section className="card">
+                    <img src={require(`../../assets/${props.data.img_slider3}`)} alt=""/>
+                    <h3>{props.data.title_slider3}</h3>
+                    <p>{props.data.text_slider3}</p>
+                </section>
             </div>
         </div>
     );
