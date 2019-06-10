d1609c2301bd786394e6b9dac4663d2afc1aa85d
unknown <xrage6@gmail.com>
2019-06-11 00:17:04 +0200

    Change HTML element in News

--- src/component/News/Index.js
+++ src/component/News/Index.js
@@ -6,21 +6,21 @@ export default function News(props) {
         <div id="news" className="news contain">
             <h2>{props.data.news}</h2>
             <div className="gallery">
-                <section className="card">
+                <div className="card">
                     <img src={require(`../../assets/${props.data.img_slider1}`)} alt=""/>
                     <h3>{props.data.title_slider1}</h3>
                     <p>{props.data.text_slider1}</p>
-                </section>
-                <section className="card">
+                </div>
+                <div className="card">
                     <img src={require(`../../assets/${props.data.img_slider2}`)} alt=""/>
                     <h3>{props.data.title_slider2}</h3>
                     <p>{props.data.text_slider2}</p>
-                </section>
-                <section className="card">
+                </div>
+                <div className="card">
                     <img src={require(`../../assets/${props.data.img_slider3}`)} alt=""/>
                     <h3>{props.data.title_slider3}</h3>
                     <p>{props.data.text_slider3}</p>
-                </section>
+                </div>
             </div>
         </div>
     );
