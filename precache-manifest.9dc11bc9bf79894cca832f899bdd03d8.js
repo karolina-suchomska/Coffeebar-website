0fe8bf76e31fce6571da592369e68c51a834dbd1
unknown <xrage6@gmail.com>
2019-06-11 10:22:11 +0200

    Updates

---
+++ precache-manifest.9dc11bc9bf79894cca832f899bdd03d8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdbb2a85f03d434f05b3d42177d0cde8",
+    "url": "/Coffeebar-website/index.html"
+  },
+  {
+    "revision": "09eab775dc85de4e608d",
+    "url": "/Coffeebar-website/static/css/2.0f7a873c.chunk.css"
+  },
+  {
+    "revision": "eed63d6063f835b171d5",
+    "url": "/Coffeebar-website/static/css/main.9694211b.chunk.css"
+  },
+  {
+    "revision": "09eab775dc85de4e608d",
+    "url": "/Coffeebar-website/static/js/2.fad4b458.chunk.js"
+  },
+  {
+    "revision": "eed63d6063f835b171d5",
+    "url": "/Coffeebar-website/static/js/main.04e8b819.chunk.js"
+  },
+  {
+    "revision": "3d263e16fe25a24dbb14",
+    "url": "/Coffeebar-website/static/js/runtime~main.68fe7968.js"
+  },
+  {
+    "revision": "64052b75676a9d212d37a6f13a1ebc1f",
+    "url": "/Coffeebar-website/static/media/bg.64052b75.svg"
+  },
+  {
+    "revision": "9175252477ae136ffc5285c65b7d9360",
+    "url": "/Coffeebar-website/static/media/coffee1.91752524.svg"
+  },
+  {
+    "revision": "1bcdef4d8b1374fe88245d8ad06409a4",
+    "url": "/Coffeebar-website/static/media/coffee2.1bcdef4d.svg"
+  },
+  {
+    "revision": "b1d7922226c20c052b103517a3a21a63",
+    "url": "/Coffeebar-website/static/media/coffee3.b1d79222.svg"
+  },
+  {
+    "revision": "38de97d01f82a4f57ed0332c3b3283bf",
+    "url": "/Coffeebar-website/static/media/img1.38de97d0.svg"
+  },
+  {
+    "revision": "600653399eda8b649b84cbf9d6e00635",
+    "url": "/Coffeebar-website/static/media/img2.60065339.svg"
+  },
+  {
+    "revision": "4bfc4b87e93f9be8908212ebbb385354",
+    "url": "/Coffeebar-website/static/media/img3.4bfc4b87.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Coffeebar-website/precache-manifest.5248e8d3722ef696dc4abc7a41306831.js"
+  "/Coffeebar-website/precache-manifest.9dc11bc9bf79894cca832f899bdd03d8.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Coffeebar-website/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Coffeebar-website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
